c0dcf1ae90b06f1ffb5a879d1ff3180869f8c4fd
siposromeo <sipos.romeo.bence@students.jedlik.eu>
2024-03-25 09:49:51 +0100

    video_url_added

--- database/migrations/2024_01_23_103029_create_autos_table.php
+++ database/migrations/2024_01_23_103029_create_autos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->integer('ferohely');
             $table->integer('loero');
             $table->string('kep_Url');
+            $table->string('video_Url');
         });
     }
 };
